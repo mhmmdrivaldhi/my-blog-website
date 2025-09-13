bc2249231e1c1bfa986e2da01bda3aa86c9f1336
Muhammad Rivaldhi <mhmmdrivaldhi10@gmail.com>
2025-09-13 22:55:44 +0700

    [Feature] modification of navbar blog

--- resources/views/livewire/blog-navigation.blade.php
+++ resources/views/livewire/blog-navigation.blade.php
@@ -1,4 +1,4 @@
-<div class="shadow-sm navbar bg-base-100">
+<div class="px-10 text-green-600 bg-gray-200 navbar">
   <div class="navbar-start">
     <div class="dropdown">
       <div tabindex="0" role="button" class="btn btn-ghost lg:hidden">
@@ -6,44 +6,38 @@
       </div>
       <ul
         tabindex="0"
-        class="p-2 mt-3 shadow menu menu-sm dropdown-content bg-base-100 rounded-box z-1 w-52">
-        <li><a>Home</a></li>
-        <li>
-          <a>Articles</a>
-          <ul class="p-2">
-            <li><a>Technology</a></li>
-            <li><a>Sport</a></li>
-            <li><a>Hiking</a></li>
-            <li><a>Photography</a></li>
-            <li><a>Travelling</a></li>
-          </ul>
-        </li>
-        <li><a>Gallery</a></li>
+        class="p-2 mt-3 font-bold text-green-600 bg-gray-100 shadow menu menu-sm dropdown-content rounded-box z-1 w-52">
+        <li><a href="#" class="transition-colors duration-300 hover:bg-green-600 hover:text-white">Home</a></li>
+        <li><a href="#" class="transition-colors duration-300 hover:bg-green-600 hover:text-white">About</a></li>
+        <li><a href="#" class="transition-colors duration-300 hover:bg-green-600 hover:text-white">Contact</a></li>
+        <li><a href="#" class="transition-colors duration-300 hover:bg-green-600 hover:text-white">Gallery</a></li>
       </ul>
     </div>
-    <a class="text-xl btn btn-ghost">R-FitTech Journey</a>
+    <a href="#" class="flex text-xl font-bold">R-FiTech</a>
   </div>
   <div class="hidden navbar-center lg:flex">
     <ul class="px-1 menu menu-horizontal">
-      <li><a>Home</a></li>
-      <li>
-        <details>
-          <summary>Articles</summary>
-          <ul class="p-2">
-            <li><a>Technology</a></li>
-            <li><a>Sport</a></li>
-            <li><a>Hiking</a></li>
-            <li><a>Photography</a></li>
-            <li><a>Travelling</a></li>
-          </ul>
-        </details>
+      <li><a href="#" class="font-bold tracking-wider after:content-[''] after:absolute after:left-0 after:-bottom-1 after:w-0 after:h-[2px] after:bg-success after:transition-all after:duration-300 hover:after:w-full">Home</a>
       </li>
-      <li><a>Gallery</a></li>
+      <li>
+        <a href="#" class="font-bold tracking-wider after:content-[''] after:absolute after:left-0 after:-bottom-1 after:w-0 after:h-[2px] after:bg-success after:transition-all after:duration-300 hover:after:w-full">About</a>
+      </li>
+      <li><a href="#" class="font-bold tracking-wider after:content-[''] after:absolute after:left-0 after:-bottom-1 after:w-0 after:h-[2px] after:bg-success after:transition-all after:duration-300 hover:after:w-full">Contact</a>
+      </li>
+      <li><a href="#" class="font-bold tracking-wider after:content-[''] after:absolute after:left-0 after:-bottom-1 after:w-0 after:h-[2px] after:bg-success after:transition-all after:duration-300 hover:after:w-full">Gallery</a>
+      </li>
     </ul>
   </div>
-  <div class="navbar-end">
-    <a href="#" class="btn">LinkedIn</a>
-    <a href="#" class="btn">Instagram</a>
-    <a href="#" class="btn">Telegram</a>
+  <div class="space-x-2 navbar-end">
+    <div class="flex items-center justify-center w-10 transition-colors duration-300 border-2 border-green-600 rounded-full aspect-square hover:bg-green-600 hover:text-white">
+        <i class="fa-brands fa-linkedin"></i>
+    </div>
+    <div class="flex items-center justify-center w-10 transition-colors duration-300 border-2 border-green-600 rounded-full aspect-square hover:bg-green-600 hover:text-white">
+        <i class="fa-brands fa-instagram"></i>
+    </div>
+    <div class="flex items-center justify-center w-10 transition-colors duration-300 border-2 border-green-600 rounded-full aspect-square hover:bg-green-600 hover:text-white">
+        <i class="fa-brands fa-telegram"></i>
+    </div>
+
   </div>
 </div>
