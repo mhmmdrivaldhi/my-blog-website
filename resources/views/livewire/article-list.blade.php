357eaecfc16d0acdec60ca9b750a046dd65d5f1d
Muhammad Rivaldhi <mhmmdrivaldhi10@gmail.com>
2025-09-30 00:33:39 +0700

    [Dev] improve the responsiveness of the article list user interface

--- resources/views/livewire/article-list.blade.php
+++ resources/views/livewire/article-list.blade.php
@@ -1,15 +1,15 @@
-<div>
-    <div class="py-10">
-        <h1 class="text-3xl font-semibold text-gray-900 font-jost">Lates Articles</h1>
+<div class="space-y-10">
+    <div class="flex flex-col items-center justify-center lg:flex-row lg:justify-start lg:items-start">
+        <h1 class="text-3xl font-semibold tracking-wider text-gray-900 font-jost">- Latest Articles -</h1>
     </div>
     <div class="space-y-5">
         @foreach ($articles as $article)
-        <div class="grid grid-cols-3 gap-5">
-            <div class="w-full col-span-1 overflow-hidden shadow-sm bg-slate-300 rounded-xl h-96">
+        <div class="grid grid-cols-1 gap-10 lg:grid-cols-3 place-items-center">
+            <div class="w-full ml-10 overflow-hidden shadow-sm md:ml-10 lg:col-span-1 bg-slate-300 rounded-xl md:h-[35rem] lg:h-[20rem] h-[15rem]">
                 <img src="{{ asset('storage/' . $article->image) }}" alt="{{ $article->title }}" class="object-cover w-full h-full">
             </div>
-            <div class="col-span-2 space-y-3">
-                <p class="px-3 py-1 text-white bg-black rounded-full w-fit">{{ $article->category->category_name }}</p>
+            <div class="col-span-2 p-5 space-y-3">
+                <small class="px-3 py-1 text-white rounded-lg bg-primary w-fit">{{ $article->category->category_name }}</small>
                 <h1 class="text-2xl font-semibold tracking-wider">{{ $article->title }}</h1>
                 <small class="text-gray-500">{{ $article->publish_date }}</small>
                 <p class="text-black">{{ $article->content }}</p>
--- resources/views/livewire/homa-page.blade.php
+++ resources/views/livewire/homa-page.blade.php
@@ -1,6 +1,6 @@
-<div class="text-green-600 lg:container">
-    <div class="grid grid-cols-2 h-[70vh] px-5 place-items-center">
-        <div class="col-span-2 px-5 py-8 lg:col-span-1 lg:py-8 space-y-7">
+<div class="space-y-5 text-green-600 lg:container">
+    <div class="grid grid-cols-2 min-h-[70vh] px-5 place-items-center">
+        <div class="col-span-2 px-5 py-8 lg:col-span-1 lg:py-3 space-y-7">
             <div class="flex flex-col items-center justify-center space-y-2 lg:items-start">
                 <div class="flex items-center justify-center bg-black border-2 border-gray-500 rounded-full w-14 aspect-square">
                 <img src="{{ asset('img/profile-thumbnail.png') }}" class="rounded-full h-14" alt="">
@@ -9,11 +9,11 @@
             <small class="font-sans text-gray-500">Tech & Sport Enthusiast</small>
             </div>
             <div class="flex flex-col items-center justify-center lg:items-start lg:justify-start gap-y-2">
-                <h1 class=" text-2xl font-bold lg:text-4xl text-medium lg:w-[35rem] w-[18rem] text-center lg:text-left lg:mt-2 tracking-wider">Find your lifestyle, unlock your passion, live healthy, grow stronger, and thrive with technology.</h1>
+                <h1 class="text-2xl font-bold md:text-3xl lg:text-4xl lg:w-[35rem] md:w-[35rem] w-[18rem] text-center lg:text-left tracking-wider">Find your lifestyle, unlock your passion, live healthy, grow stronger, and thrive with technology.</h1>
             </div>
         </div>
-        <div class="col-span-2 p-5 lg:p-12 lg:col-span-1">
-            <img src="{{ asset('img/thumbnail-home.jpg') }}" class="object-cover w-full rounded-2xl" alt="">
+        <div class="col-span-2 p-5 lg:p-10 lg:col-span-1">
+            <img src="{{ asset('img/thumbnail-home.jpg') }}" class="object-cover w-full h-full rounded-2xl" alt="">
         </div>
     </div>
 
