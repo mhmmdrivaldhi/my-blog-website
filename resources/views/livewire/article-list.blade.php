844c18d712b7a11b9491f738fad26133634bb439
Muhammad Rivaldhi <mhmmdrivaldhi10@gmail.com>
2025-10-17 20:54:08 +0700

    [Dev] Improve UI Homepage with AOS Animation

--- resources/views/livewire/article-list.blade.php
+++ resources/views/livewire/article-list.blade.php
@@ -4,7 +4,7 @@
     </div>
     <div class="space-y-5">
         @foreach ($articles as $article)
-        <div class="grid grid-cols-1 gap-5 p-5 lg:grid-cols-3 place-items-center lg:p-0">
+        <div class="grid grid-cols-1 gap-5 p-5 lg:grid-cols-3 place-items-center lg:p-0" data-aos="zoom-in" data-aos-duration="1000">
             <div class="w-full ml-10 overflow-hidden shadow-sm md:ml-10 lg:col-span-1 bg-slate-300 rounded-xl md:h-[35rem] lg:h-[20rem] h-[14rem]">
                 <img src="{{ asset('storage/' . $article->image) }}" alt="{{ $article->title }}" class="object-cover w-full h-full">
             </div>
--- resources/views/livewire/gallery-blog.blade.php
+++ resources/views/livewire/gallery-blog.blade.php
@@ -1,15 +1,15 @@
 <div class="gap-5 p-4 columns-1 sm:columns-2 lg:columns-3 xl:columns-4">
     @foreach ($images as $image)
-        <div class="relative mb-4 overflow-hidden rounded-lg break-inside-avoid group">
-            <img src="{{ asset('img/' . $image['file']) }}" alt="{{ $image['title'] }}"
-                class="object-cover w-full h-auto transition-transform duration-500 group-hover:scale-105">
-            <div
-                class="absolute inset-0 flex items-end p-4 transition-opacity duration-300 opacity-0 bg-black/50 group-hover:opacity-100">
-                <div>
-                    <h3 class="font-semibold text-white">{{ $image['title'] }}</h3>
-                    <p class="text-sm text-gray-300">{{ $image['category'] }}</p>
+            <div class="relative mb-4 overflow-hidden rounded-lg break-inside-avoid group" data-aos="flip-left" data-aos-easing="ease-out-cubic" data-aos-duration="2000">
+                <img src="{{ asset('img/' . $image['file']) }}" alt="{{ $image['title'] }}"
+                    class="object-cover w-full h-auto transition-transform duration-500 group-hover:scale-105">
+                <div
+                    class="absolute inset-0 flex items-end p-4 transition-opacity duration-300 opacity-0 bg-black/50 group-hover:opacity-100">
+                    <div>
+                        <h3 class="font-semibold text-white">{{ $image['title'] }}</h3>
+                        <p class="text-sm text-gray-300">{{ $image['category'] }}</p>
+                    </div>
                 </div>
             </div>
-        </div>
     @endforeach
 </div>
